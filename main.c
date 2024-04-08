67c3f77b8246c5b43388c00efc7e6b1dbf81da36
charlotte weston <charlotte.weston@epitech.eu>
2024-04-08 16:46:16 +0200

    [+] added coding style errors in main.c

--- main.c
+++ main.c
@@ -7,7 +7,12 @@
 
 int main(void)
 {
-    int a = 0;
-
+    int a = 0, b = 1;
+    for (int i = 0; i < 1; i++) {
+        for (int j = 0; j < 1; j++) {
+            if (j == 5)
+                return;
+        }
+    }
     return 0;
 }
